f8ffe98a5958bae9f20e1cd70a599b17bef80001
DESKTOP-URPJ43V\Max11mus <koseymax@gmail.com>
2021-12-30 08:58:20 +0200

    The formatting and refactoring of the code is done.

---
+++ anagrams/src/main/java/ua/com/foxminded/lms/anagrams/App.java
@@ -0,0 +1,39 @@
+package ua.com.foxminded.lms.anagrams;
+import java.util.Scanner;
+
+/**
+ *Task 1 - Anagrams 
+ *Write an application that reverses all the words of input text:
+ *E.g. "abcd efgh" => "dcba hgfe"
+ *All non-letter symbols should stay on the same places:
+ *E.g. "a1bcd efg!h" => "d1cba hgf!e"
+ *Use Latin alphabet for test only.
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    
+    	
+    	 
+    	 String input, output;
+    	    	
+    	 Menu menu = new Menu();
+    	 Anagrams anagram = new Anagrams();
+    	 
+    	 menu.outGreetingMsg();
+    	 
+    	 System.out.println("Please, enter the text:");
+    	 Scanner in = new Scanner(System.in);
+    	 input = in.nextLine();
+    	 in.close();
+    	 output = anagram.reverse(input);
+    	 System.out.println("Output text: ");
+    	 System.out.println(output);
+    	 
+    	 menu.outFarewellMsg();
+    	 
+    	 
+    }
+}
---
+++ anagrams/src/main/java/ua/com/foxminded/lms/anagrams/Menu.java
@@ -0,0 +1,31 @@
+package ua.com.foxminded.lms.anagrams;
+
+public class Menu {
+
+	public void outGreetingMsg () {
+		
+		String greetings = "\t \t \t HELLO !!! \r\n It's an "
+			+ "application that reverses all the words of input text:\r\n"
+			+ "\r\n"
+			+ "  E.g. \"abcd efgh\" => \"dcba hgfe\"\r\n"
+			+ "\r\n"
+			+ "All non-letter symbols should stay on the same places:\r\n"
+			+ "\r\n"
+			+ "  E.g. \"a1bcd efg!h\" => \"d1cba hgf!e\"\r\n"
+			+ "\r\n"
+			+ "Use Latin alphabet for test only. \r\n";
+		System.out.println(greetings);
+		
+		}
+	
+
+	public void outFarewellMsg() {
+		
+		String farewell = "\t \t \t GOODBYE !!!"; 
+		System.out.println(farewell);
+		
+	}
+	
+	
+	
+}
