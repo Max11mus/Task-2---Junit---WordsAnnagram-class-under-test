f439918541e84905738168949fc5768bda2c6947
DESKTOP-URPJ43V\Max11mus <koseymax@gmail.com>
2022-01-12 11:31:57 +0200

    Added STRING_SPLITED_COMPLETELY constant.

--- anagrams/src/main/java/ua/com/foxminded/lms/wordsannagram/WordsAnnagram.java
+++ anagrams/src/main/java/ua/com/foxminded/lms/wordsannagram/WordsAnnagram.java
@@ -2,37 +2,37 @@ package ua.com.foxminded.lms.wordsannagram;
 
 public final class WordsAnnagram {
 	public static final String DELIMITER = " ";
-	
-	public  String reverseOnlyLetters(String input) {
+	public static final int STRING_SPLITED_COMPLETELY = -1;
+
+	public String reverseOnlyLetters(String input) {
 		String result = "";
-		String [] words =  input.split(DELIMITER, -1);
-		
+		String[] words = input.split(DELIMITER, STRING_SPLITED_COMPLETELY);
+
 		if (words.length != 0) {
-		result = reverseWord(words[0]);
+			result = reverseWord(words[0]);
 			for (int i = 1; i < words.length; i++) {
-			words[i] = reverseWord(words[i]);
-			result = result.concat(DELIMITER);
-			result = result.concat(words[i]);
+				words[i] = reverseWord(words[i]);
+				result = result.concat(DELIMITER);
+				result = result.concat(words[i]);
 			}
-	    }	
-	return result;
+		}
+		return result;
 	}
 
-	
-	private  String reverseWord(String input) {
-		char [] inputChars = input.toCharArray();
-		char [] resultChars = input.toCharArray();
-		
-		for (int i = 0, k = (inputChars.length - 1) ; i < resultChars.length; i++) {
-			while ( (k>=1) && (!Character.isLetter(inputChars[k])) ) {
+	private String reverseWord(String input) {
+		char[] inputChars = input.toCharArray();
+		char[] resultChars = input.toCharArray();
+
+		for (int i = 0, k = (inputChars.length - 1); i < resultChars.length; i++) {
+			while ((k >= 1) && (!Character.isLetter(inputChars[k]))) {
 				k--;
-				}
-			if ( (Character.isLetter(resultChars[i])) && (k>=0) ) {
+			}
+			if ((Character.isLetter(resultChars[i])) && (k >= 0)) {
 				resultChars[i] = inputChars[k];
 				k--;
-			} 
+			}
 		}
-		return  new String(resultChars);
+		return new String(resultChars);
 	}
 
 }
